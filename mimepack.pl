470b563808e96508e29fc7b690845743dfb8b783
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2012-08-31 11:31:44 +0200

    UPDATED: mime_pack/2: documentation, extending mime/3 input handling, fixed and documented charset issues.

--- mimepack.pl
+++ mimepack.pl
@@ -1,11 +1,9 @@
-/*  $Id$
-
-    Part of SWI-Prolog
+/*  Part of SWI-Prolog
 
     Author:        Jan Wielemaker
-    E-mail:        J.Wielemaker@cs.vu.nl
+    E-mail:        J.Wielemaker@vu.nl
     WWW:           http://www.swi-prolog.org
-    Copyright (C): 1985-2011, University of Amsterdam
+    Copyright (C): 1985-2012, University of Amsterdam
 			      VU University Amsterdam
 
     This program is free software; you can redistribute it and/or
@@ -36,13 +34,13 @@
 :- use_module(mimetype).
 :- use_module(html_write).
 :- use_module(library(lists)).
+:- use_module(library(error)).
 
 /** <module> Create a MIME message
 
 Simple and partial implementation of MIME   encoding. MIME is covered by
-RFC 2045 which I've read from
-
-	http://www.cis.ohio-state.edu/cgi-bin/rfc/rfc2045.html
+RFC 2045. This library is used by  e.g., http_post_data/3 when using the
+form_data(+ListOfData) input specification.
 
 MIME decoding is now  arranged  through   library(mime)  from  the  clib
 package, based on the  external  librfc2045   library.  Most  likely the
@@ -55,9 +53,63 @@ Packing however is a lot simpler then parsing.
 %	Pack a number of inputs into a MIME package using a specified or
 %	generated boundary. The  generated  boundary   consists  of  the
 %	current time in milliseconds  since  the   epoch  and  10 random
-%	hexadecimal numbers.
+%	hexadecimal numbers. Inputs is a  list   of  _documents_ that is
+%	added to the mime message.  Each element is one of:
 %
-%	@bug	Does not validate that the boundary is unique.
+%	  * Name = Value
+%	  Name the document. This emits a header of the form below. The
+%	  =filename= is present if Value is of the form file(File).
+%	  Value may be any of remaining value specifications.
+%
+%	    ==
+%	    Content-Disposition: form-data; name="Name"[; filename="<File>"
+%	    ==
+%
+%	  * html(Tokens)
+%	  Tokens is a list of HTML tokens as produced by html//1. The
+%	  token list is emitted using print_html/1.
+%
+%	  * file(File)
+%	  Emit the contents of File. The =|Content-type|= is derived
+%	  from the File using file_mime_type/2.  If the content-type
+%	  is =|text/_|=, the file data is copied in text mode, which
+%	  implies that it is read in the default encoding of the system
+%	  and written using the encoding of the Out stream.  Otherwise
+%	  the file data is copied binary.
+%
+%	  * stream(In, Len)
+%	  Content is the next Len units from In.  Data is copied using
+%	  copy_stream_data/3. Units is bytes for binary streams and
+%	  characters codes for text streams.
+%
+%	  * stream(In)
+%	  Content of the stream In, copied using copy_stream_data/2.
+%	  This is often used with memory files (see new_memory_file/1).
+%
+%	  * mime(Attributes, Value, [])
+%	  Create a MIME header from Attributes and add Value, which can
+%	  be any of remaining values of this list. Attributes may
+%	  contain type(ContentType) and/or character_set(CharSet).  This
+%	  can be used to give a content-type to values that otherwise
+%	  do not have a content-type.  For example:
+%
+%	    ==
+%	    mime([type(text/html)], '<b>Hello World</b>', [])
+%	    ==
+%
+%	  * mime([], '', Parts)
+%	  Creates a nested multipart MIME message.  Parts is passed
+%	  as Inputs to a recursive call to mime_pack/2.
+%
+%	  * Atomic
+%	  Atomic values are passed to write/1. This embeds simple atoms
+%	  and numbers.
+%
+%	@param	Out is a stream opened for writing. Typically, it should
+%		be opened in text mode using UTF-8 encoding.
+%
+%	@bug	Does not validate that the boundary does not appear in
+%		any of the input documents.
 
 mime_pack(Inputs, OutputStream, Boundary) :-
 	make_boundary(Inputs, Boundary),
@@ -73,7 +125,7 @@ pack_list([H|T], Out, Boundary) :-
 
 pack(X, _Out) :-
 	var(X), !,
-	throw(error(instantiation_error, _)).
+	instantiation_error(X).
 pack(Name=Value, Out) :- !,
 	(   Value = file(FileName)
 	->  format(Out, 'Content-Disposition: form-data; name="~w"; filename="~w"\r\n',
@@ -91,12 +143,19 @@ pack(file(File), Out) :- !,
 	),
 	format(Out, 'Content-Type: ~w\r\n\r\n', [Type]),
 	(   Type = text/_
-	->  OpenOptions = []
-	;   OpenOptions = [type(binary)]
-	),
-	setup_call_cleanup(open(File, read, In, OpenOptions),
-			   copy_stream_data(In, Out),
-			   close(In)).
+	->  setup_call_cleanup(
+		open(File, read, In),
+		copy_stream_data(In, Out),
+		close(In))
+	;   stream_property(Out, encoding(OldEncoding)),
+	    setup_call_cleanup(
+		set_stream(Out, encoding(octet)),
+		setup_call_cleanup(
+		    open(File, read, In, [type(binary)]),
+		    copy_stream_data(In, Out),
+		    close(In)),
+		set_stream(Out, encoding(OldEncoding)))
+	).
 pack(stream(In, Len), Out) :- !,
 	format(Out, '\r\n', []),
 	copy_stream_data(In, Out, Len).
@@ -105,7 +164,7 @@ pack(stream(In), Out) :- !,
 	copy_stream_data(In, Out).
 pack(mime(Atts, Data, []), Out) :- !,		% mime_parse compatibility
 	write_mime_attributes(Atts, Out),
-	write(Out, Data).
+	pack(Data, Out).
 pack(mime(_Atts, '', Parts), Out) :-
 	make_boundary(Parts, Boundary),
 	format('Content-type: multipart/mixed; boundary=~w\r\n\r\n',
@@ -118,8 +177,7 @@ pack(Atom, Out) :-
 pack(Value, _) :-
 	throw(error(type_error(mime_part, Value), _)).
 
-write_mime_attributes([], Out) :- !,
-	format(Out, '\r\n', []).
+write_mime_attributes([], _) :- !.
 write_mime_attributes(Atts, Out) :-
 	select(type(Type), Atts, A1), !,
 	(   select(character_set(CharSet), A1, A2)
