SWI-Prolog/packages-http

commit 6824d3c73e2992d32b70ccc4cd72611c28cad546

FIXED: http_in_session/1 if the session is opened using http_open_session/2 and automatic session management is disabled.

http_session.pl
@@ -197,17 +197,27 @@ http_session_id(SessionID) :-
 %	@see http_session_id/1
 
 http_in_session(SessionID) :-
-	(   nb_current(http_session_id, ID),
-	    ID \== []
-	->  true
-	;   http_current_request(Request),
-	    memberchk(session(ID), Request),
-	    b_setval(http_session_id, ID)
-	;   b_setval(http_session_id, no_session),
-	    fail
-	),
+	nb_current(http_session_id, ID),
+	ID \== [], !,
 	ID \== no_session,
 	SessionID = ID.
+http_in_session(SessionID) :-
+	http_current_request(Request),
+	http_in_session(Request, SessionID).
+
+http_in_session(Request, SessionID) :-
+	memberchk(session(ID), Request),
+	b_setval(http_session_id, ID), !,
+	SessionID = ID.
+http_in_session(Request, SessionID) :-
+	memberchk(cookie(Cookies), Request),
+	session_setting(cookie(Cookie)),
+	memberchk(Cookie=SessionID0, Cookies),
+	peer(Request, Peer),
+	valid_session_id(SessionID0, Peer), !,
+	b_setval(http_session_id, ID), !,
+	SessionID = ID.
+
 
 %%	http_session(+RequestIn, -RequestOut, -SessionID) is semidet.
 %

commit 7d8c32bd4f813c409a5066e9a92c87081a4a6be7

FIXED: http_in_session/1 loops if session is expired.  Bug introduced in 6824d3c73e2992d32b70ccc4cd72611c28cad546.

http_session.pl
@@ -489,7 +489,7 @@ http_close_session(SessionId, Expire) :-
 	    ;	true
 	    ),
 	    (	Expire == true
-	    ->	expire_session_cookie(SessionId)
+	    ->	expire_session_cookie
 	    ;	true
 	    ),
 	    retractall(current_session(SessionId, _)),
@@ -503,11 +503,9 @@ http_close_session(SessionId, Expire) :-
 %%	expire_session_cookie(+SessionId) is det.
 %
 %	Emit a request to delete a session  cookie. This is only done if
-%	http_close_session/1 is called from a   handler executing inside
-%	the session and the handler is still in `header mode'.
+%	http_close_session/1 is still in `header mode'.
 
-expire_session_cookie(SessionId) :-
-	http_in_session(SessionId),
+expire_session_cookie :-
 	in_header_state,
 	session_setting(cookie(Cookie)),
 	session_setting(path(Path)), !,
@@ -515,7 +513,7 @@ expire_session_cookie(SessionId) :-
 		expires=Tue, 01-Jan-1970 00:00:00 GMT; \c
 		path=~w\r\n',
 	       [Cookie, Path]).
-expire_session_cookie(_).
+expire_session_cookie.
 
 in_header_state :-
 	current_output(CGI),