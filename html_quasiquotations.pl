e20aef857fd0c3d83c2fe2838a4c8cc32a1c1676
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2015-02-16 11:39:19 +0100

    FIXED: Case handling of HTML5 Quasi Quotations.  Requires updating library(sgml) as well.

--- html_quasiquotations.pl
+++ html_quasiquotations.pl
@@ -78,7 +78,7 @@ html(Content, Vars, Dict, DOM) :-
 	    Content, In,
 	    load_html(In, DOM0,
 		      [ max_errors(0),
-			case_sensitive_attributes(true)
+			case_preserving_attributes(true)
 		      ])),
 	xml_content(QQDict, DOM0, DOM).
 
