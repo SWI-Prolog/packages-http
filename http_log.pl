76a76e1aadab8da177ff79b36c68e8b692bfc2f1
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2016-08-16 09:01:43 +0200

    ADDED: library(http/http_log): http_logrotate/1 and http_schedule_logrotate/2

--- http_log.pl
+++ http_log.pl
@@ -37,10 +37,15 @@
 	  [ http_log_stream/1,		% -Stream
 	    http_log/2,			% +Format, +Args
 	    http_log_close/1,		% +Reason
-	    post_data_encoded/2		% ?Bytes, ?Encoded
+	    post_data_encoded/2,	% ?Bytes, ?Encoded
+	    http_logrotate/1,		% +Options
+	    http_schedule_logrotate/2	% +When, +Options
 	  ]).
 :- use_module(library(http/http_header)).
 :- use_module(library(settings)).
+:- use_module(library(option)).
+:- use_module(library(error)).
+:- use_module(library(debug)).
 :- use_module(library(broadcast)).
 
 :- setting(http:logfile, callable, 'httpd.log',
@@ -326,24 +331,6 @@ log_completed(Code, Status, Bytes, Id, CPU0, _) :-
 log_completed(_,_,_,_,_,_).
 
 
-%%	log_check_deleted(+Stream) is semidet.
-%
-%	If the link-count of the stream has   dropped  to zero, the file
-%	has been deleted/moved. In this case the  log file is closed and
-%	log_check_deleted/6 will open a  new   one.  This  provides some
-%	support for cleaning up the logfile   without  shutting down the
-%	server.
-%
-%	@see logrotate(1) to manage logfiles on Unix systems.
-
-log_check_deleted(Stream) :-
-	stream_property(Stream, nlink(Links)),
-	Links == 0, !,
-	http_log_close(log_file_deleted),
-	fail.
-log_check_deleted(_).
-
-
 log(Code, ok, Bytes, Id, CPU, Stream) :- !,
 	format(Stream, 'completed(~q, ~q, ~q, ~q, ok).~n',
 	       [ Id, CPU, Bytes, Code ]).
@@ -373,3 +360,255 @@ map_exception(http_reply(Reply), Reply).
 map_exception(http_reply(Reply, _), Reply).
 map_exception(error(existence_error(http_location, Location), _Stack),
 	      error(404, Location)).
+
+
+		 /*******************************
+		 *	LOGROTATE SUPPORT	*
+		 *******************************/
+
+%%	log_check_deleted(+Stream) is semidet.
+%
+%	If the link-count of the stream has   dropped  to zero, the file
+%	has been deleted/moved. In this case the  log file is closed and
+%	log_check_deleted/6 will open a  new   one.  This  provides some
+%	support for cleaning up the logfile   without  shutting down the
+%	server.
+%
+%	@see logrotate(1) to manage logfiles on Unix systems.
+
+log_check_deleted(Stream) :-
+	stream_property(Stream, nlink(Links)),
+	Links == 0, !,
+	http_log_close(log_file_deleted),
+	fail.
+log_check_deleted(_).
+
+%%	http_logrotate(+Options) is det.
+%
+%	Rotate the available log files. Note that  there are two ways to
+%	deal with the rotation of log files:
+%
+%	  1. Use the OS log rotation facility. In that case the OS must
+%	  (1) move the logfile and (2) have something calling
+%	  http_log_close/1 to close the (moved) file and make this
+%	  server create a new one on the next log message.  If
+%	  library(http/http_unix_daemon) is used, closing is
+%	  achieved by sending SIGHUP or SIGUSR1 to the process.
+%
+%	  2. Call this predicate at scheduled intervals.  This can
+%	  be achieved by calling http_schedule_logrotate/2 in the
+%	  context of library(http/http_unix_daemon) which schedules
+%	  the maintenance actions.
+%
+%	Options:
+%
+%	  - min_size(+Bytes)
+%	  Do not rotate if the log file is smaller than Bytes.
+%	  The default is 1Mbytes.
+%	  - keep_logs(+Count)
+%	  Number of rotated log files to keep (default 10)
+%	  - compress_logs(+Format)
+%	  Compress the log files to the given format.
+%	  - background(+Boolean)
+%	  If `true`, rotate the log files in the background.
+
+http_logrotate(Options) :-
+	select_option(background(true), Options, Options1), !,
+	thread_create(http_logrotate(Options1), _,
+		      [ alias('__logrotate'),
+			detached(true)
+		      ]).
+http_logrotate(Options) :-
+	option(keep_logs(Keep), Options, 10),
+	option(compress_logs(Format), Options, gzip),
+	compress_extension(Format, ZExt),
+	log_file_and_ext(Base, Ext),
+	(   log_too_small(Base, Ext, Options)
+	->  true
+	;   rotate_logs(Base, Ext, ZExt, Keep)
+	).
+
+rotate_logs(Base, Ext, ZExt, N1) :-
+	N1 > 0, !,
+	N0 is N1 - 1,
+	old_log_file(Base, Ext, N0, ZO, Old),
+	(   exists_file(Old)
+	->  new_log_file(Base, Ext, N1, ZO, ZExt, ZN, New),
+	    rename_log_file(ZO, Old, ZN, New)
+	;   true
+	),
+	rotate_logs(Base, Ext, ZExt, N0).
+rotate_logs(_, _, _, _).
+
+rename_log_file(ZExt, Old, ZExt, New) :- !,
+	debug(logrotate, 'Rename ~p --> ~p', [Old, New]),
+	rename_file(Old, New).
+rename_log_file('', Old, ZExt, New) :-
+	file_name_extension(NoExt, ZExt, New),
+	debug(logrotate, 'Rename ~p --> ~p', [Old, NoExt]),
+	rename_file(Old, NoExt),
+	debug(logrotate, 'Closing log file', []),
+	http_log_close(logrotate),
+	compress_extension(Format, ZExt),
+	debug(logrotate, 'Compressing (~w) ~p', [Format, NoExt]),
+	compress_file(NoExt, Format).
+
+old_log_file(Base, Ext, N, ZExt, File) :-
+	log_file(Base, Ext, N, File0),
+	(   compress_extension(_, ZExt),
+	    file_name_extension(File0, ZExt, File1),
+	    exists_file(File1)
+	->  File = File1
+	;   ZExt = '',
+	    File = File0
+	).
+
+new_log_file(Base, Ext, N, '', '', '', File) :- !,
+	log_file(Base, Ext, N, File).
+new_log_file(Base, Ext, N, '', ZExt, ZExt, File) :- !,
+	log_file(Base, Ext, N, File0),
+	file_name_extension(File0, ZExt, File).
+new_log_file(Base, Ext, N, ZExt, _, ZExt, File) :-
+	log_file(Base, Ext, N, File0),
+	file_name_extension(File0, ZExt, File).
+
+log_file(Base, Ext, 0, File) :- !,
+	file_name_extension(Base, Ext, File).
+log_file(Base, Ext, N, File) :-
+	atomic_list_concat([Base, -, N], Base1),
+	file_name_extension(Base1, Ext, File).
+
+log_file_and_ext(Base, Ext) :-
+	setting(http:logfile, Term),
+	catch(absolute_file_name(Term, File,
+				 [ access(exist)
+				 ]), _, fail),
+	file_name_extension(Base, Ext, File).
+
+log_too_small(Base, Ext, Options) :-
+	DefMin is 1024*1024,
+	option(min_size(MinBytes), Options, DefMin),
+	file_name_extension(Base, Ext, File),
+	(   exists_file(File)
+	->  size_file(File, Bytes),
+	    Bytes < MinBytes,
+	    debug(logrotate, '~w has ~D bytes; not rotating', [File, Bytes])
+	;   debug(logrotate, '~w does not exist; not rotating', [File])
+	).
+
+%%	compress_file(+File, +Format)
+%
+%	Compress a file according  to   Format.  Currently only supports
+%	gzip.
+
+compress_file(File, Format) :-
+	(   compress_extension(Format, Ext)
+	->  true
+	;   domain_error(compress_format, Format)
+	),
+	file_name_extension(File, Ext, ZFile),
+	catch(setup_call_cleanup(
+		  gzopen(ZFile, write, Out, [type(binary)]),
+		  setup_call_cleanup(
+		      open(File, read, In, [type(binary)]),
+		      copy_stream_data(In, Out),
+		      close(In)),
+		  close(Out)),
+	      Error,
+	      (	print_message(error, Error),
+		catch(delete_file(Out), _, true),
+		throw(Error)
+	      )),
+	delete_file(File).
+
+compress_extension(gzip, gz).
+
+:- dynamic
+	scheduled_logrotate/2.	% Schedule, Options
+
+%%	http_schedule_logrotate(When, Options)
+%
+%	Schedule log rotation based on maintenance broadcasts.  When
+%	is one of:
+%
+%	  - daily(Hour:Min)
+%	  Run each day at Hour:Min.  Min is rounded to a multitude
+%	  of 5.
+%	  - weekly(Day, Hour:Min)
+%	  Run at the given Day and Time each week.  Day is either a
+%	  number 1..7 (1 is Monday) or a weekday name or abbreviation.
+%	  - monthly(DayOfTheMonth, Hour:Min)
+%	  Run each month at the given Day (1..31).  Note that not all
+%	  months have all days.
+%
+%	This  must  be  used   with   a    timer   that   broadcasts   a
+%	maintenance(_,_) message (see broadcast/1). Such a timer is part
+%	of library(http/http_unix_daemon).
+
+http_schedule_logrotate(When, Options) :-
+	listen(maintenance(_,_), http_consider_logrotate),
+	compile_schedule(When, Schedule),
+	retractall(scheduled_logrotate(_,_)),
+	asserta(scheduled_logrotate(Schedule, Options)).
+
+compile_schedule(Var, _) :-
+	var(Var), !,
+	instantiation_error(Var).
+compile_schedule(daily(Time0), daily(Time)) :-
+	compile_time(Time0, Time).
+compile_schedule(weekly(Day0, Time0), weekly(Day, Time)) :-
+	compile_weekday(Day0, Day),
+	compile_time(Time0, Time).
+compile_schedule(monthly(Day, Time0), monthly(Day, Time)) :-
+	must_be(between(0, 31), Day),
+	compile_time(Time0, Time).
+
+compile_time(HH:MM0, HH:MM) :-
+	must_be(between(0, 23), HH),
+	must_be(between(0, 59), MM0),
+	MM is ((MM0+4)/5)*5.
+
+compile_weekday(N, _) :-
+	var(N), !,
+	instantiation_error(N).
+compile_weekday(N, N) :-
+	integer(N), !,
+	must_be(between(1,7), N).
+compile_weekday(Name, N) :-
+	downcase_atom(Name, Lwr),
+	(   day(N, Name),
+	    sub_atom(Name, 0, _, _, Lwr)
+	->  !
+	;   domain_error(day, Name)
+	).
+
+%%	http_consider_logrotate
+%
+%	Perform a log rotation if the schedule is met
+
+http_consider_logrotate :-
+	scheduled_logrotate(Schedule, Options),
+	get_time(Now),
+	scheduled(Schedule, Now), !,
+	http_logrotate([background(true)|Options]).
+
+scheduled(daily(HH:MM), Now) :-
+	stamp_date_time(Now, DateTime, local),
+	date_time_value(time, DateTime, time(HH,MM,_)).
+scheduled(weekly(Day, Time), Now) :-
+	stamp_date_time(Now, DateTime, local),
+	date_time_value(date, DateTime, Date),
+	day_of_the_week(Date, Day),
+	scheduled(daily(Time), Now).
+scheduled(monthly(Day, Time), Now) :-
+	stamp_date_time(Now, DateTime, local),
+	date_time_value(day, DateTime, Day),
+	scheduled(daily(Time), Now).
+
+day(1, mon).
+day(2, tue).
+day(3, wed).
+day(4, thu).
+day(5, fri).
+day(6, sat).
+day(7, sun).
