e146697ab7aef5a2cd1b49373048c9a49808cfd8
Wouter Beek <wouter@triply.cc>
2017-06-10 12:16:28 +0200

    DOC: Improved documentation of option value_string_as/1.
    Copied the documentation for json_read/3 to apply to predicate
    json_read_dict/3 as well (which has the same behavior).  It is
    specifically important that the allowed values of this option
    are now mentioned.

--- json.pl
+++ json.pl
@@ -924,7 +924,11 @@ is_json_pair(Options, Name=Value) :-
 %     * false(+FalseTerm)
 %     Default the atom `false`
 %     * value_string_as(+Type)
-%     Type defaults to `string`, producing a packed string object.
+%     Prolog type used for strings used as value.  Default
+%     is =atom=.  The alternative is =string=, producing a
+%     packed string object.  Please note that =codes= or
+%     =chars= would produce ambiguous output and is therefore
+%     not supported.
 
 json_read_dict(Stream, Dict) :-
     json_read_dict(Stream, Dict, []).
