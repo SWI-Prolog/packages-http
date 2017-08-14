7eaf92e5a5472bb0d89d0f42423e19e9e611b800
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2017-08-14 21:10:49 +0200

    ENHANCED: Issue#101: provide default UTF-8 encoding for Turtle derived formats and allow extending the mapping.  Wouter Beek.

--- http_header.pl
+++ http_header.pl
@@ -3,7 +3,7 @@
     Author:        Jan Wielemaker
     E-mail:        J.Wielemaker@vu.nl
     WWW:           http://www.swi-prolog.org
-    Copyright (c)  2002-2016, University of Amsterdam
+    Copyright (c)  2002-2017, University of Amsterdam
                               VU University Amsterdam
     All rights reserved.
 
@@ -80,8 +80,8 @@
 
 :- multifile
     http:status_page/3,             % +Status, +Context, -HTML
-    http:post_data_hook/3.          % +Data, +Out, +HdrExtra
-
+    http:post_data_hook/3,          % +Data, +Out, +HdrExtra
+    http:mime_type_encoding/2.       % +MimeType, -Encoding
 
 % see http_update_transfer/4.
 
@@ -676,17 +676,32 @@ http_update_encoding(Header, Encoding, Header) :-
         ;   sub_atom(Type, _, _, _, 'utf-8')
         )
     ->  Encoding = utf8
+    ;   http:mime_type_encoding(Type, Encoding)
+    ->  true
     ;   mime_type_encoding(Type, Encoding)
     ).
 http_update_encoding(Header, octet, Header).
 
 %!  mime_type_encoding(+MimeType, -Encoding) is semidet.
 %
-%   Encoding is the (default) character encoding for MimeType.
+%   Encoding is the (default) character encoding for MimeType. Hooked by
+%   http:mime_type_encoding/2.
 
-mime_type_encoding('application/json',        utf8).
-mime_type_encoding('application/jsonrequest', utf8).
-mime_type_encoding('application/x-prolog',    utf8).
+mime_type_encoding('application/json',         utf8).
+mime_type_encoding('application/jsonrequest',  utf8).
+mime_type_encoding('application/x-prolog',     utf8).
+mime_type_encoding('application/n-quads',      utf8).
+mime_type_encoding('application/n-triples',    utf8).
+mime_type_encoding('application/sparql-query', utf8).
+mime_type_encoding('application/trig',         utf8).
+
+%!  http:mime_type_encoding(+MimeType, -Encoding) is semidet.
+%
+%   Encoding is the (default) character encoding   for MimeType. This is
+%   used for setting the encoding for HTTP  replies after the user calls
+%   format('Content-type: <MIME type>~n'). This hook   is  called before
+%   mime_type_encoding/2. This default  defines  `utf8`   for  JSON  and
+%   Turtle derived =|application/|= MIME types.
 
 
 %!  http_update_connection(+CGIHeader, +Request, -Connection, -Header)
