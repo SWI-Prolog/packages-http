7342e9de95fdc1cc4a27f7fd7af84fb137379a3d
Jan Wielemaker <J.Wielemaker@vu.nl>
2020-01-06 09:25:59 +0100

    Explicitly load library(lists).

--- json.pl
+++ json.pl
@@ -70,7 +70,7 @@
 :- use_module(library(memfile)).
 :- use_module(library(error)).
 :- use_module(library(option)).
-:- use_module(library(debug)).
+:- use_module(library(lists)).
 
 :- use_foreign_library(foreign(json)).
 
