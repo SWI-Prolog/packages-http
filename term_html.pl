f3a86883fe4666904cbd4ad605f53b613b312867
Jan Wielemaker <J.Wielemaker@vu.nl>
2022-12-21 10:18:31 +0100

    FIXED: Issue#120: term//2 (term to HTML) rendering of infix terms if these contain "member" terms with arity 2.

--- term_html.pl
+++ term_html.pl
@@ -451,7 +451,7 @@ left_space(_,_,_) -->
 
 xfy_list(Term, Name, List),
     compound(Term),
-    compound_name_arguments(Term, _, [A,B]) =>
+    compound_name_arguments(Term, Name, [A,B]) =>
     List = [A|T],
     xfy_list(B, Name, T).
 xfy_list(Term, _, List) =>
